d47ce3c477f0054a3ec5b8e5c51fbacd41d14579
Anup <anuppochchhi25@gmail.com>
2024-05-16 09:15:44 +0530

    first commit

---
+++ src/main/java/com/Factorial/Code/FactorialCodeApplication.java
@@ -0,0 +1,13 @@
+package com.Factorial.Code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactorialCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactorialCodeApplication.class, args);
+	}
+
+}
